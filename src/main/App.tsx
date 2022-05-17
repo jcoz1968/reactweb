83b51f9e1a009c7c3c0387dc056b8ce67c452cc6
jeffery.cosby <jeffery.cosby@omes.ok.gov>
2022-05-17 10:27:20 -0500

    setup project, module 2

---
+++ src/main/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="providing houses all over the world" />
+    </div>
+  );
+}
+
+export default App;
