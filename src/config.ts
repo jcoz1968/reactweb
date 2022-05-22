034f8894b263fd20d489c7e48bc48ec2284d3696
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2022-05-22 11:57:13 -0500

    formatting incoming data, api status message

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,11 @@ const config = {
     baseApiUrl: 'https://localhost:4000'
 };
 
+const currencyFormatter = Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0
+});
+
 export default config;
+export { currencyFormatter };
