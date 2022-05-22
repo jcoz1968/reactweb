79f25daaf8ebd8ecab6cdf687fdfdb207e168fbb
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2022-05-22 12:24:44 -0500

    router

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import Header from './Header';
 import './App.css';
 import HouseList from "../house/HouseList" ;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container">
-      <Header subtitle="providing houses all over the world" />
-      <HouseList />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header subtitle="providing houses all over the world" />
+        <Routes>
+          <Route path='/' element={<HouseList/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
