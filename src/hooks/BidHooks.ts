9a9b37ffe5eaf8aea496fcba86fba12bd1bc24b2
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2022-05-24 10:58:54 -0500

    Bid Hooks

---
+++ src/hooks/BidHooks.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import config from "../config";
+import { Bid } from "../types/bid";
+import Problem from "../types/problem";
+
+const useFetchBids = (houseId: number) => {
+    return useQuery<Bid[], AxiosError<Problem>>(["bids", houseId]),
+        () => axios.get(`${config.baseApiUrl}/house/${houseId}/bids`)
+            .then((resp) => resp.data);
+};
+
+const useAddBid = () => {
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError<Problem>, Bid>(
+      (b) => axios.post(`${config.baseApiUrl}/house/${b.houseId}/bids`, b),
+      {
+        onSuccess: (resp, bid) => {
+          queryClient.invalidateQueries(["bids", bid.houseId]);
+        },
+      }
+    );
+  };
+
+  export { useFetchBids, useAddBid };
